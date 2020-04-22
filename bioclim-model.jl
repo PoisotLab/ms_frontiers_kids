8b80ea2e3f20d2940d139dc90b8c525f7eb63b6d
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-04-22 11:37:55 -0400

    Upload BIOCLIM model
    Co-Authored-By: Timothée Poisot <timothee.poisot@umontreal.ca>

---
+++ bioclim-model.jl
@@ -0,0 +1,76 @@
+import Pkg
+Pkg.activate(".")
+
+using GBIF
+using SimpleSDMLayers
+using StatsBase
+using Plots
+
+# Get the GBIF taxon
+raccoon = GBIF.taxon("Procyon lotor", rank=:SPECIES, strict=true)
+
+# Get the initial occurrences
+raccoon_occ = GBIF.occurrences(raccoon, "limit" => 100)
+
+# Get the rest of the occurrences
+while length(raccoon_occ) <= 2000
+    try
+        GBIF.occurrences!(raccoon_occ)
+    catch#
+        break
+    end
+end
+
+# Hide the occurrences who do not match quality
+filter!(GBIF.have_ok_coordinates, raccoon_occ)
+length(raccoon_occ)
+
+scatter(longitudes(raccoon_occ), latitudes(raccoon_occ))
+
+# Get the temperature and precipitation
+temperature = clip(SimpleSDMLayers.worldclim(1), raccoon_occ)
+precipitation = clip(SimpleSDMLayers.worldclim(12), raccoon_occ)
+
+heatmap(temperature)
+scatter!(longitudes(raccoon_occ), latitudes(raccoon_occ), lab="")
+
+# Extract the values of the layers at the positions
+histogram(temperature[raccoon_occ])
+histogram(precipitation[raccoon_occ])
+
+# Bioclim model
+function _bioclim_score(x::Number)
+    if isnan(x)
+        return NaN
+    end
+    @assert 0. ≤ x ≤ 1.
+    if x ≥ 0.5
+        return 2(1-x)
+    else
+        return 2x
+    end
+end
+
+function bioclim(layer::T, records::GBIFRecords) where {T <: SimpleSDMLayer}
+    bioclim_prediction = similar(layer)
+    qf = StatsBase.ecdf(filter(!isnan, layer[records]))
+    bioclim_prediction.grid = _bioclim_score.(qf.(layer.grid))
+    return bioclim_prediction
+end
+
+temp_pred = bioclim(temperature, raccoon_occ)
+prec_pred = bioclim(precipitation, raccoon_occ)
+
+function Base.min(l1::T, l2::T) where {T <: SimpleSDMLayer}
+    SimpleSDMLayers._layers_are_compatible(l1, l2)
+    min_layer = similar(l1)
+    for i in eachindex(l1.grid)
+        if !isnan(l1[i])
+            min_layer[i] = min(l1[i], l2[i])
+        end
+    end
+    return min_layer
+end
+
+sdm_raccoon = min(temp_pred, prec_pred)
+heatmap(sdm_raccoon, c=:cividis, frame=:box, clim=(0,1))
